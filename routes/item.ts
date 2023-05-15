5ea3a5be5958a57f92f5549c06d05cc481196aa1
Subham Agarwal <subhamagarwal400@gmail.com>
2023-05-15 07:42:47 +0530

    update

--- routes/item.ts
+++ routes/item.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const router =express.Router();
 import {getItem,createItem,updateItem,deleteItem} from '../controller/itemController';
+import authenticate from '../src/index';
 
 
 router.get("/",getItem)
